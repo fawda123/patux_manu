71085c462ac742013cb86c015c57ca227aba93b2
fawda123 <mbafs2012@gmail.com>
2015-08-06 16:30:59 -0500

    performance tables added

---
+++ R/funcs.R
@@ -0,0 +1,38 @@
+######
+# helper functions
+
+######
+#r.squared function
+#created for data from weighted regression, but works for all models
+#residuals are observed - predicted, but taken from model objects (see 'epc_mods.R')
+rsq.fun<-function(resid,obs){
+  
+  require(Metrics)
+  
+  # get complete cases
+  toeval <- data.frame(resid, obs)
+  toeval <- na.omit(toeval)
+  
+  ssr<-sum(toeval$resid^2)
+  sst<-sum(se(toeval$obs,mean(toeval$obs)))
+  
+  return(1 - (ssr/sst))
+
+}
+
+######
+#variant of rmse fun in Metrics package but handles na values
+#resid is obs - predicted
+rmse.fun<-function(resid){
+  
+  out<-sqrt(mean(resid^2,na.rm=T))
+    
+  return(out) 
+  
+}
+
+######
+# formatting of values in S expressions
+form_fun <- function(x, rnd_val = 2, dig_val = 2, nsm_val = 2) {
+  format(round(x, rnd_val), digits = dig_val, nsmall = nsm_val)
+  }
