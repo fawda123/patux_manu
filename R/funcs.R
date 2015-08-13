5d5a7d9ea589ad531b8bd3e1de47b2409067ebeb
fawda123 <mbafs2012@gmail.com>
2015-08-13 16:09:29 -0500

    added flow-norm results to fig 4

--- R/funcs.R
+++ R/funcs.R
@@ -115,4 +115,12 @@ lm.fun <- function(ts1, ts2, origin = FALSE, alph = 0.05){
 # formatting of values in S expressions
 form_fun <- function(x, rnd_val = 2, dig_val = 2, nsm_val = 2) {
   format(round(x, rnd_val), digits = dig_val, nsmall = nsm_val)
-  }
+}
+
+######
+# get legend from an existing ggplot object
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
